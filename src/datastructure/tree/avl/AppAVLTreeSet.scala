ad8fcffc84619984d065b59f3520eb593f4d5369
Luis Cesar Maiaru <lmaiaru@proveedor.personal.com.ar>
2016-11-07 04:33:24 -0300

    Separating the + operator.

--- src/datastructure/tree/avl/AppAVLTreeSet.scala
+++ src/datastructure/tree/avl/AppAVLTreeSet.scala
@@ -2,7 +2,7 @@ package datastructure.tree.avl
 
 object AppAVLTreeSet extends App {
   val t1 = AVLTreeSet[Int]()
-  val t2 = t1+1+2+3+4+5+6+7+8+9+10
+  val t2 = t1 + 1 + 2 + 3 +4 + 5+ 6 + 7 + 8 +9 + 10
   println(t2)
   println(t2.toList)
   val t3 = t2-1-2-3-4-5-6-7-8
