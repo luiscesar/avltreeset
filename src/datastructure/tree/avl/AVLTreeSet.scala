0941aeafa433860cc24bfec609183c310323e714
luiscesar <luiscesar.maiaru@gmail.com>
2017-05-26 14:17:57 +0100

    Update AVLTreeSet.scala

--- src/datastructure/tree/avl/AVLTreeSet.scala
+++ src/datastructure/tree/avl/AVLTreeSet.scala
@@ -14,31 +14,31 @@ object AVLTreeSet {
   }
 }
 
-sealed abstract class AVLTreeSet[T<%Ordered[T]](var h:Int) {
-	def +(v:T):AVLTreeSet[T]
-	def -(v:T):AVLTreeSet[T]
-	def depth:Int
+sealed abstract class AVLTreeSet[T<%Ordered[T]](var h: Int) {
+	def +(v:T): AVLTreeSet[T]
+	def -(v:T): AVLTreeSet[T]
+	def depth: Int
 	def size: Int
-	def isEmpty:Boolean
-	def nonEmpty:Boolean
-	def toList:List[T]  
+	def isEmpty: Boolean
+	def toList: List[T]
 	
 	def contains(v: T) : Boolean = this match {
     case EmptyAVLTreeSet() => false
-    case NonEmptyAVLTreeSet(x,sf,l,r) if (v == x)=> true
     case NonEmptyAVLTreeSet(x,sf,l,r) if (v < x) => l.contains(v)
-    case NonEmptyAVLTreeSet(x,sf,l,r) if (v > x) => r.contains(v)    
+    case NonEmptyAVLTreeSet(x,sf,l,r) if (v > x) => r.contains(v)
+    case _ => true
   }
 
   protected def balance(t: AVLTreeSet[T]) : AVLTreeSet[T] = t match {
     case EmptyAVLTreeSet() => t
     case NonEmptyAVLTreeSet(x,sf,l,r) if (sf == 0) => t
-    case NonEmptyAVLTreeSet(x,sf,l,r) if (sf == -1) => t
-    case NonEmptyAVLTreeSet(x,sf,l,r) if (sf == 1)=> t
-    case NonEmptyAVLTreeSet(x,sf1,NonEmptyAVLTreeSet(y,sf2,t1,t2),r) if ( (sf1 == -2) && (sf2 == -1) ) => t.rightRotation()
-    case NonEmptyAVLTreeSet(x,sf1,l,NonEmptyAVLTreeSet(y,sf2,t1,t2)) if ( (sf1 == 2) && (sf2 == 1) ) => t.leftRotation()
-    case NonEmptyAVLTreeSet(x,sf1,NonEmptyAVLTreeSet(y,sf2,t1,NonEmptyAVLTreeSet(z,_,t2,t3)),r) if ( (sf1 == -2) && (sf2 == 0) ) => t.rightDoubleRotation()
-    case NonEmptyAVLTreeSet(x,sf1,l,NonEmptyAVLTreeSet(y,sf2,NonEmptyAVLTreeSet(z,_,t1,t2),t3)) if ( (sf1 == 2) && (sf2 == 0) ) => t.leftDoubleRotation()
+    case NonEmptyAVLTreeSet(x,sf,l,r) if (sf.abs == 1) => t
+    case NonEmptyAVLTreeSet(x,sf1,NonEmptyAVLTreeSet(y,sf2,t1,t2),r) if ( ((sf1 == -2) && (sf2 == -1)) ||
+      ((sf1 == -2) && (sf2 == 0))) => t.rightRotation()
+    case NonEmptyAVLTreeSet(x,sf1,l,NonEmptyAVLTreeSet(y,sf2,t1,t2)) if ( ((sf1 == 2) && (sf2 == 1)) ||
+      ((sf1 == 2) && (sf2 == 0)) ) => t.leftRotation()
+    case NonEmptyAVLTreeSet(x,sf1,NonEmptyAVLTreeSet(y,sf2,t1,NonEmptyAVLTreeSet(z,_,t2,t3)),r) if ( (sf1 == -2) && (sf2 == 1) ) => t.leftRightRotation()
+    case NonEmptyAVLTreeSet(x,sf1,l,NonEmptyAVLTreeSet(y,sf2,NonEmptyAVLTreeSet(z,_,t1,t2),t3)) if ( (sf1 == 2) && (sf2 == -1) ) => t.rightLeftRotation()
   }
  	
   protected def rightRotation() : AVLTreeSet[T] = this match {
@@ -53,12 +53,12 @@ sealed abstract class AVLTreeSet[T<%Ordered[T]](var h:Int) {
     case _ => this
   }
   
-  protected def rightDoubleRotation() : AVLTreeSet[T] = this match {
+  protected def leftRightRotation() : AVLTreeSet[T] = this match {
     case NonEmptyAVLTreeSet(x,_,l,r) => {val t1 = l.leftRotation();val t2 = new NonEmptyAVLTreeSet(x,r.h-t1.h,t1,r);t2.rightRotation()}
     case _ => this
   }
 
-  protected def leftDoubleRotation() : AVLTreeSet[T] = this match {
+  protected def rightLeftRotation() : AVLTreeSet[T] = this match {
     case NonEmptyAVLTreeSet(x,_,l,r) => {val t1 = r.rightRotation();val t2 = new NonEmptyAVLTreeSet(x,t1.h-l.h,l,t1);t2.leftRotation()}
     case _ => this
   }
@@ -73,7 +73,6 @@ private case class EmptyAVLTreeSet[T<%Ordered[T]]() extends AVLTreeSet[T](-1) {
 	def depth:Int = 0
 	def size: Int = 0
 	def isEmpty: Boolean = true
-	def nonEmpty:Boolean = false
 	def toList: List[T] = List[T]()
 }
 
@@ -83,25 +82,36 @@ private case class NonEmptyAVLTreeSet[T<%Ordered[T]](v:T,private var sf:Int,left
 	def depth:Int = 1+AVLTreeSet.max(left.h,right.h)
 	def size:Int = 1+left.size+right.size
 	def isEmpty:Boolean = false
-	def nonEmpty:Boolean = true
-	
+
 	def toList: List[T] = this match {
     case NonEmptyAVLTreeSet(x,_,l,r) => l.toList:::x::r.toList
   }
 
   def +(v:T): AVLTreeSet[T] = this match {
-    case NonEmptyAVLTreeSet(x,sf,l,r) if (v < x) => {val t = l+v; val t1= new NonEmptyAVLTreeSet(x,r.h-t.h,t,r); balance(t1);}
-    case NonEmptyAVLTreeSet(x,sf,l,r) if (v > x) => {val t = r+v; val t1 = new NonEmptyAVLTreeSet(x,t.h-l.h,l,t); balance(t1)}
+    case NonEmptyAVLTreeSet(x,sf,l,r) if (v < x) => {
+      val t = l+v;
+      val t1= new NonEmptyAVLTreeSet(x,r.h-t.h,t,r);
+      balance(t1);
+    }
+    case NonEmptyAVLTreeSet(x,sf,l,r) if (v > x) => {
+      val t = r+v;
+      val t1 = new NonEmptyAVLTreeSet(x,t.h-l.h,l,t);
+      balance(t1)}
     case _ => this
   }
 	
   def -(v: T): AVLTreeSet[T] = this match {
     case NonEmptyAVLTreeSet(x,_,l,r) if (v < x) => {val t=l-v; val t1 = new NonEmptyAVLTreeSet(x,r.h-t.h,t,r); balance(t1)}
     case NonEmptyAVLTreeSet(x,_,l,r) if (v > x) => {val t = r-v; val t1 = new NonEmptyAVLTreeSet(x,t.h-l.h,l,t);balance(t1)}
-    case NonEmptyAVLTreeSet(x,_,EmptyAVLTreeSet(),EmptyAVLTreeSet()) if (x == v) => new EmptyAVLTreeSet()
-    case NonEmptyAVLTreeSet(x,_,EmptyAVLTreeSet(),r) if (x == v) => r
-    case NonEmptyAVLTreeSet(x,_,l,EmptyAVLTreeSet()) if (x == v) => l
-    case NonEmptyAVLTreeSet(x,_,l,r) if (x == v) => {var y = inorderPredecesor(l); val t=l-y; val t1=new NonEmptyAVLTreeSet(y,r.h-t.h,t,r);balance(t1)}
+    case NonEmptyAVLTreeSet(x,_,EmptyAVLTreeSet(),EmptyAVLTreeSet()) => new EmptyAVLTreeSet()
+    case NonEmptyAVLTreeSet(x,_,EmptyAVLTreeSet(),r) => r
+    case NonEmptyAVLTreeSet(x,_,l,EmptyAVLTreeSet()) => l
+    case NonEmptyAVLTreeSet(x,_,l,r) => {
+      var y = inorderPredecesor(l);
+      val t=l-y;
+      val t1 = new NonEmptyAVLTreeSet(y,r.h-t.h,t,r);
+      balance(t1)
+    }
   }
   
   protected def inorderPredecesor(t:AVLTreeSet[T]) : T = t match {
@@ -113,5 +123,6 @@ private case class NonEmptyAVLTreeSet[T<%Ordered[T]](v:T,private var sf:Int,left
 }
 
 case class AVLTreeSetException(msg:String)  extends Exception
+
 
 
